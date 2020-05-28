27c05a3403c35014d8fa4997925e9bc4616e795c
BogdanDevBst <bn.workdev@gmail.com>
2020-05-28 16:09:05 +0100

    Three algorithms done

--- addBorder/addBorder.ts
+++ addBorder/addBorder.ts
@@ -1,5 +1,14 @@
-function addBorder(picture: string[]): string[] {
+const addBorder = (picture: string[]): any => {
+  const wall = "*".repeat(picture[0].length + 2);
 
-}
+  picture.unshift(wall);
+  picture.push(wall);
 
-// console.log(addBorder(["abc", "ded"]));
+  for (let i = 1; i < picture.length - 1; i++) {
+    picture[i] = "*".concat(picture[i], "*");
+  }
+
+  return picture;
+};
+
+console.log(addBorder(["abc", "ded"]));
