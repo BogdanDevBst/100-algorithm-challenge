bf75726a43670d96bc647905f538b61d2d6eebfa
BogdanDevBst <bn.workdev@gmail.com>
2020-05-29 15:29:51 +0100

    done

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,8 @@
-function addTwoDigits(n: any): number {
+const addTwoDigits = (n: any): number => {
+  const nums = n.toString().split("");
+  console.log(nums);
 
-}
+  return parseInt(nums[0]) + parseInt(nums[1]) + parseInt(nums[2]);
+};
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(223));
