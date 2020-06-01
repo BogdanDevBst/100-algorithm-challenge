ed81e82a48a9c8445a3f64623b7fbe6061df8614
BogdanDevBst <bn.workdev@gmail.com>
2020-06-01 17:39:10 +0100

    done

--- areSimilar/areSimilar.ts
+++ areSimilar/areSimilar.ts
@@ -1,7 +1,28 @@
-function areSimilar(a: number[], b: number[]): boolean {
-   
-}
+const areSimilar = (firstArr: number[], secondArr: number[]): boolean => {
+  const thirdArr: number[] = [];
+  let fourthArr: number[] = [];
+
+  if (firstArr.toString() === secondArr.toString()) {
+    return true;
+  }
+
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      thirdArr.push(firstArr[i]);
+      fourthArr.push(secondArr[i]);
+    }
+  }
+
+  fourthArr = fourthArr.reverse();
+
+  if (thirdArr.length === 2 && thirdArr.toString() === fourthArr.toString()) {
+    return true;
+  }
+  return false;
+};
 
 console.log(areSimilar([1, 2, 3], [1, 2, 3]));
 console.log(areSimilar([1, 2, 3], [2, 1, 3]));
+console.log(areSimilar([1, 2, 2, 1], [2, 1, 1, 2]));
 console.log(areSimilar([1, 2, 2], [2, 1, 1]));
+console.log(areSimilar([1, 2, 3, -4], [2, 1, 3, -4]));
--- arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
+++ arrayMaxConsecutiveSum/arrayMaxConsecutiveSum.ts
@@ -1,5 +1,22 @@
-function arrayMaxConsecutiveSum(inputArray: number[], k: number): number {
+const arrayMaxConsecutiveSum = (inputArray: number[], k: number): number => {
+  let sum: number = 0;
+  let max: number = 0;
 
-}
+  for (let i = 0; i < k; i++) {
+    sum += inputArray[i];
+  }
 
-console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6], 2));
+  max = sum;
+
+  for (let i = k; i < inputArray.length; i++) {
+    sum -= inputArray[i - k];
+    sum += inputArray[i];
+    if (sum > max) {
+      max = sum;
+    }
+  }
+
+  return max;
+};
+
+console.log(arrayMaxConsecutiveSum([2, 3, 5, 1, 6, 2, 4, 5, 6, 4], 3));
